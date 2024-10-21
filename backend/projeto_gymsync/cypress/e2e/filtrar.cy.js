2b7ae405a0fb2488a0a89c02cf268100122d2df8
sam-abreu <168447703+sam-abreu@users.noreply.github.com>
2024-10-21 15:01:26 -0300

    Implementei o teste automarizado da função de filtrar exercícios

---
+++ backend/projeto_gymsync/cypress/e2e/filtrar.cy.js
@@ -0,0 +1,14 @@
+describe('template spec', () => {
+    it('teste 1 filtrar treino', () => {
+        cy.visit('http://127.0.0.1:8000/treino/44/add_exercicio/')
+        cy.wait(2000)
+        cy.get('#filtro').select('Costas')
+        cy.wait(2000)
+        cy.get('#repeticoes2').type(10)
+        cy.wait(2000)
+        cy.get('#carga2').type(80)
+        cy.wait(2000)
+        cy.get('[data-grupo="Costas"] > form > .btn').click()
+        cy.wait(2000)
+    })
+  })
