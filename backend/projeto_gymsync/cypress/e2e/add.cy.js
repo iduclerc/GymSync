b88204bb5e2eab16a001ae8d077d849b4f718076
Lucas Calabria <lvcalabria@gmail.com>
2024-10-21 16:03:19 -0300

    teste de adicionar exercicio ao treino ajeitado
    .

--- backend/projeto_gymsync/cypress/e2e/add.cy.js
+++ backend/projeto_gymsync/cypress/e2e/add.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
     it('teste 1 add exercicio', () => {
   
-      cy.visit('http://127.0.0.1:8000/treino/37/add_exercicio/')
+      cy.visit('http://127.0.0.1:8000/treino/44/add_exercicio/')
       cy.wait(1000) // espera 3s
       cy.get('#treino1').type('treino 1')
       cy.wait(1000)
@@ -12,7 +12,7 @@ describe('template spec', () => {
     })
     it('teste 2 add exercicio', () => {
   
-        cy.visit('http://127.0.0.1:8000/treino/37/add_exercicio/')
+        cy.visit('http://127.0.0.1:8000/treino/44/add_exercicio/')
         cy.wait(1000) // espera 3s
         cy.get('#treino1').type('treino 1')
         cy.wait(1000)
