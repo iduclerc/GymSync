21987f300ff11eae40ad541d65728e955a40cdab
sam-abreu <168447703+sam-abreu@users.noreply.github.com>
2024-10-22 11:09:30 -0300

    Terminando o teste do cronometro
    Terminei o trabalho inacabado de ontem do teste automatizado do cronnômetro

--- backend/projeto_gymsync/cypress/e2e/cronometro.cy.js
+++ backend/projeto_gymsync/cypress/e2e/cronometro.cy.js
@@ -1,7 +1,12 @@
 describe('template spec', () => {
     it('teste 1 cronometro', () => {
-        cy.visit('http://localhost:8000/lista_exercicio')
+        cy.visit('http://127.0.0.1:8000/treino/64/')
         cy.wait(1000)
-        // PRECISAMOS TERMINAR ESTE TESTE
+        cy.get(':nth-child(1) > .cronometro-container > .start-btn').click()
+        cy.wait(5000)
+        cy.get(':nth-child(1) > .cronometro-container > .stop-btn').click()
+        cy.wait(1000)
+        cy.get(':nth-child(1) > .cronometro-container > .reset-btn').click()
+        cy.wait(2000)
     })
   })
