89d3814764e3142b3571079dfbc08967871d2080
sam-abreu <168447703+sam-abreu@users.noreply.github.com>
2024-10-21 23:50:12 -0300

    Teste automatizado cronometro
    Comecei agora de noite a trabalhar nesse teste mas está dando algum bug que não consegui resolver, mesmo tentando muito tempo,  então, como está tarde, vou resolver amanhã pela manhã.

---
+++ backend/projeto_gymsync/cypress/e2e/cronometro.cy.js
@@ -0,0 +1,7 @@
+describe('template spec', () => {
+    it('teste 1 cronometro', () => {
+        cy.visit('http://localhost:8000/lista_exercicio')
+        cy.wait(1000)
+        // PRECISAMOS TERMINAR ESTE TESTE
+    })
+  })
